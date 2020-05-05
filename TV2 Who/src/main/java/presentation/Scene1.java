ce1213a266612a5ae727d38121518d025d3fa648
Tesniem <tes_7583@hotmail.com>
2020-05-05 10:56:05 +0200

    kan skifte scene

--- TV2 Who/src/main/java/presentation/Scene1.java
+++ TV2 Who/src/main/java/presentation/Scene1.java
@@ -1,23 +1,28 @@
 package presentation;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class Scene1 extends AnchorPane {
 
+
     public Scene1(){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scene1.fxml"));
-
         fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
+
+       fxmlLoader.setController(this);
 
         try {
             fxmlLoader.load();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 
 }
